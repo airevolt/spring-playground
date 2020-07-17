b9097354b09139633b493d077bc1443807639cf8
airvolt <vee10128@gmail.com>
2020-07-17 15:46:29 -0400

    Initial commit

---
+++ src/main/java/com/example/Galvanize/GalvanizeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Galvanize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalvanizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalvanizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Galvanize/GalvanizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Galvanize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalvanizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
