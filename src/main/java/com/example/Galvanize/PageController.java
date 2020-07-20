7b14bdd34a74f94519c535eef4280c8c31466e80
airvolt <vee10128@gmail.com>
2020-07-20 16:42:48 -0400

    Body Param

--- src/main/java/com/example/Galvanize/PageController.java
+++ src/main/java/com/example/Galvanize/PageController.java
@@ -1,22 +1,40 @@
 package com.example.Galvanize;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PageController {
 
-    @RequestMapping("/{length}/{width}/{height}")
-    public String calculate(@PathVariable(value="length") String length
-                            ,@PathVariable(value="width") String width
-                            ,@PathVariable(value="height") String height) {
-        int l = Integer.valueOf(length);
-        int w = Integer.valueOf(width);
-        int h = Integer.valueOf(height);
-        String volume = String.valueOf(l*w*h);
-        return "The volume of a is: " + volume;
+//    @RequestMapping("/{length}/{width}/{height}")
+//    public String calculate(@PathVariable(value="length") String length
+//                            ,@PathVariable(value="width") String width
+//                            ,@PathVariable(value="height") String height) {
+//        int l = Integer.valueOf(length);
+//        int w = Integer.valueOf(width);
+//        int h = Integer.valueOf(height);
+//        String volume = String.valueOf(l*w*h);
+//        return "The volume of a is: " + volume;
+//    }
+
+    @PostMapping("/math/area")
+    public String area(@RequestParam(value = "radius") String radius,
+                       @RequestParam(value = "width") String width,
+                       @RequestParam(value = "height") String height,
+                       @RequestParam(value = "type") String type
+                       ){
+                int w = Integer.valueOf(width);
+                int h = Integer.valueOf(height);
+                int r = Integer.valueOf(radius);
+                double pi = 3.14;
+
+                if(type.contains("circle")){
+                    return String.valueOf(pi * r * r);
+                }else if(type.contains("triangle")){
+                    return String.valueOf(w * h);
+                }
+
+                return "No type found";
+
     }
 
 }
